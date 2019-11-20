34df1ca1f5d7713250770c7d232250732dff1047
MVSaiKrishna <42886268+MVSaiKrishna@users.noreply.github.com>
2019-11-20 13:09:50 +0530

    Add files via upload

--- OpenBookTest/first.s
+++ OpenBookTest/first.s
@@ -1,4 +1,8 @@
-	
+; Assembly code to plot circle
+;x=r*sin(theta)
+;y=r*cos(theta)
+;radius=150
+;centre=(320,240)
 	THUMB
 	  AREA     pixels, CODE, READONLY
       EXPORT __main
@@ -7,13 +11,13 @@
 	  IMPORT printMsg4p
       ENTRY 
 __main  FUNCTION
-		MOV R3,#10
+		MOV R3,#91
 		MOV R4,#1
 
 		VLDR.F32 S22,=0 ; initial angle
-		VLDR.F32 S1,=30 ; increment
+		VLDR.F32 S1,=4 ; increment
 		
-		VLDR.F32 S23,=10; radius
+		VLDR.F32 S23,=150; radius
 		
 	;Degrees to Radian Conversion
 		
@@ -22,6 +26,9 @@ __main  FUNCTION
 		VDIV.F32 S29,S31,S30 ; Pi/180
 		
 		VLDR.F32 S4,=2 ; to calculate 2*i, ((2*i)+1) and ((2*i)-1)
+		
+		VLDR.F32 S18,=320; x shift
+		VLDR.F32 S19,=240; y shift
 
 COUNT	CMP R4,R3			;Compare 'i' and 'n'
 		BLE Angle
@@ -33,19 +40,24 @@ Angle	VMUL.F32 S2,S2,S29 ; (pi/180)*angle - given angle is converted to radians
 	;Every term has to be multiplied by x^2 for next iteration 
 		VMUL.F32 S3,S2,S2 ; x*x
 		
-		VCVT.U32.F32 S22,S22
+		;VCVT.U32.F32 S22,S22
 		VMOV.F32 R0,S22
-		;BL printMsg4p	 ; Refer to ARM Procedure calling standards.
+		;BL printMsg	 ; Refer to ARM Procedure calling standards.
+		
 		BL SIN
 		VMUL.F32 S24,S0,S23
+		VADD.F32 S24,S24,S18
 		VCVT.U32.F32 S24,S24
-		VMOV.F32 R1,S24	
-		;BL printMsg4p	 ; Refer to ARM Procedure calling standards.
+		VMOV.F32 R0,S24	
+		;BL printMsg	 ; Refer to ARM Procedure calling standards.
+		
 		BL COS
 		VMUL.F32 S26,S20,S23
+		VADD.F32 S26,S26,S19
 		VCVT.U32.F32 S26,S26
-		VMOV.F32 R2,S26	
-		BL printMsg4p	 ; Refer to ARM Procedure calling standards.
+		VMOV.F32 R1,S26	
+		BL printMsg2p	 ; Refer to ARM Procedure calling standards.
+		
 		VADD.F32 S22,S22,S1	;
 		VMOV.F32 S2,S22;
 		ADD R4,R4,#1
@@ -53,7 +65,7 @@ Angle	VMUL.F32 S2,S2,S29 ; (pi/180)*angle - given angle is converted to radians
 		
 		
 
-SIN		MOV R0,#10 ; No of terms in a series 
+SIN		MOV R5,#10 ; No of terms in a series 
 		MOV R1,#1 ; counting variable n (To repeat the loop)
 
 		VMOV.F32 S11,S2 ; Using S11 to store the intermediate result of sinx
@@ -63,11 +75,11 @@ SIN		MOV R0,#10 ; No of terms in a series
 		VLDR.F32 S6,=1; increment value
 		
 
-COUNT1   CMP R1,R0			;Compare 'i' and 'n' 
+COUNT1  CMP R1,R5			;Compare 'i' and 'n' 
 		BLE LOOP1			;if i < n goto LOOP
 		BX LR
 			
-LOOP1    VMUL.F32 S7,S4,S5	; 2*i
+LOOP1   VMUL.F32 S7,S4,S5	; 2*i
 		VADD.F32 S8,S7,S6	; (2*i)+1
 		
 		;Calculating next term for sinx 
@@ -84,7 +96,7 @@ LOOP1    VMUL.F32 S7,S4,S5	; 2*i
 		ADD R1,R1,#1		;Increment the counter variable 'i'
 		B COUNT1				;Again goto comparision
 		
-COS		MOV R0,#10 ; No of terms in a series 
+COS		MOV R5,#10 ; No of terms in a series 
 		MOV R1,#1 ; counting variable n (To repeat the loop)
 		
 		VLDR.F32 S20,=1 ; Cosx  result is stored in s20 register
@@ -93,11 +105,11 @@ COS		MOV R0,#10 ; No of terms in a series
 		VLDR.F32 S5,=1; i value
 		VLDR.F32 S6,=1; increment value
 		
-COUNT2   CMP R1,R0			;Compare 'i' and 'n' 
+COUNT2	CMP R1,R5			;Compare 'i' and 'n' 
 		BLE LOOP2			;if i < n goto LOOP
 		BX LR
 			
-LOOP2    VMUL.F32 S7,S4,S5	; 2*i
+LOOP2   VMUL.F32 S7,S4,S5	; 2*i
 		VSUB.F32 S12,S7,S6	; (2*i)-1
 		
 		;Calculating next term for sinx 
