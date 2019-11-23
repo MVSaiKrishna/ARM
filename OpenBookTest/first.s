ae41bf7ed09898477b8c652ca4b1d7b9a8344cf9
MVSaiKrishna <42886268+MVSaiKrishna@users.noreply.github.com>
2019-11-23 18:03:54 +0530

    Update first.s

--- OpenBookTest/first.s
+++ OpenBookTest/first.s
@@ -11,8 +11,8 @@
 	  IMPORT printMsg4p
       ENTRY 
 __main  FUNCTION
-		MOV R3,#91
-		MOV R4,#1
+		MOV R3,#91	; Incrementing the angle from to 0 to 360 with an increment of 4 degrees 
+		MOV R4,#1	; Initial i value 
 
 		VLDR.F32 S22,=0 ; initial angle
 		VLDR.F32 S1,=4 ; increment
@@ -27,6 +27,7 @@ __main  FUNCTION
 		
 		VLDR.F32 S4,=2 ; to calculate 2*i, ((2*i)+1) and ((2*i)-1)
 		
+		;shifting the centre to (320,240)
 		VLDR.F32 S18,=320; x shift
 		VLDR.F32 S19,=240; y shift
 
@@ -41,27 +42,27 @@ Angle	VMUL.F32 S2,S2,S29 ; (pi/180)*angle - given angle is converted to radians
 		VMUL.F32 S3,S2,S2 ; x*x
 		
 		;VCVT.U32.F32 S22,S22
-		VMOV.F32 R0,S22
-		;BL printMsg	 ; Refer to ARM Procedure calling standards.
+		VMOV.F32 R0,S22		; R0 is updated with new angle after every iteration
+		;BL printMsg	 ; 
 		
-		BL SIN
-		VMUL.F32 S24,S0,S23
-		VADD.F32 S24,S24,S18
-		VCVT.U32.F32 S24,S24
-		VMOV.F32 R0,S24	
-		;BL printMsg	 ; Refer to ARM Procedure calling standards.
+		BL SIN			; Calling Sin(theta) function
+		VMUL.F32 S24,S0,S23	; r*sin(theta)
+		VADD.F32 S24,S24,S18	; y-shift+r*sin(theta)
+		VCVT.U32.F32 S24,S24	; Converting the result to Integer value from IEEE-754 format
+		VMOV.F32 R0,S24		; Moving the integer value to R0 register  
+		;BL printMsg	 ; 
 		
-		BL COS
-		VMUL.F32 S26,S20,S23
-		VADD.F32 S26,S26,S19
-		VCVT.U32.F32 S26,S26
-		VMOV.F32 R1,S26	
-		BL printMsg2p	 ; Refer to ARM Procedure calling standards.
+		BL COS			; Calling Sin(theta) function
+		VMUL.F32 S26,S20,S23	; r*cos(theta)
+		VADD.F32 S26,S26,S19	; x-shift+r*cos(theta)
+		VCVT.U32.F32 S26,S26	; onverting the result to Integer value from IEEE-754 format
+		VMOV.F32 R1,S26		; Moving the integer value to R1 register
+		BL printMsg2p	 	; Calling printMsg c function to the values of (Xi,Yi) 	
 		
-		VADD.F32 S22,S22,S1	;
-		VMOV.F32 S2,S22;
-		ADD R4,R4,#1
-		B COUNT
+		VADD.F32 S22,S22,S1	; Increment the angle by 4 degrees 
+		VMOV.F32 S2,S22;	; Storing the new angle calculated after every iteration in S2 register
+		ADD R4,R4,#1		; Counter to repeat the loop 
+		B COUNT			; goto COUNT after every iteration until the condition is satisfied 
 		
 		
 
@@ -77,7 +78,7 @@ SIN		MOV R5,#10 ; No of terms in a series
 
 COUNT1  CMP R1,R5			;Compare 'i' and 'n' 
 		BLE LOOP1			;if i < n goto LOOP
-		BX LR
+		BX LR			; Return to the main program once the sin(theta) is calculated 
 			
 LOOP1   VMUL.F32 S7,S4,S5	; 2*i
 		VADD.F32 S8,S7,S6	; (2*i)+1
@@ -107,7 +108,7 @@ COS		MOV R5,#10 ; No of terms in a series
 		
 COUNT2	CMP R1,R5			;Compare 'i' and 'n' 
 		BLE LOOP2			;if i < n goto LOOP
-		BX LR
+		BX LR			; Return to the main program once the cos(theta) is calculated 
 			
 LOOP2   VMUL.F32 S7,S4,S5	; 2*i
 		VSUB.F32 S12,S7,S6	; (2*i)-1
